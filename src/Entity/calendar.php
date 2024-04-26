098e83edbc45cbbb1f26bf9a1628d59a1c632dbb
EsoraKyugo <tom.7trouillet@gmail.com>
2024-04-26 14:08:27 +0200

    changerment pour bdd

---
+++ src/Entity/calendar.php
@@ -0,0 +1,31 @@
+// src/Entity/Calendar.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="calendar")
+ */
+class Calendar
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    // Getters and Setters
+}
